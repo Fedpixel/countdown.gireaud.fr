2d11702a5454f6e75cd2883aa36a14f58aacce1e
Quentin GIREAUD <quentin.gireaud@be-ys.cloud>
2023-04-28 15:16:14 +0200

    Adding progressbar

--- javascript.js
+++ javascript.js
@@ -1,5 +1,6 @@
 const countdown = () => {
   const countDate = new Date('April 28, 2023 17:00:00').getTime();
+  const start = new Date('April 24, 2023 09:00:00').getTime();
   const now = new Date().getTime();
   const gap = countDate - now;
 
@@ -7,11 +8,13 @@ const countdown = () => {
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
+  const percentage = (100 * ( now - start ) / ( countDate - start ))
 
   const textDay = Math.floor(gap / day);
   const textHour = Math.floor((gap % day) / hour);
   const textMinute = Math.floor((gap % hour) / minute);
   const textSecond = Math.floor((gap % minute) / second);
+  const textPercentage = Math.round(percentage);
 
   const daysElement = document.getElementById('days');
   const hoursElement = document.getElementById('hours');
@@ -24,7 +27,12 @@ const countdown = () => {
   secondsElement.innerHTML = textSecond;
 
   document.title = textDay + "jours " + textHour + "h " + textMinute + "min " + textSecond + "s"; 
-  
+
+  var elem = document.getElementById("myBar");
+  elem.style.width = textPercentage + "%";
+  elem.style.backgroundColor = "#04AA6D";
+  elem.innerHTML = textPercentage + "%";
+
   if (gap < 0) {
     clearInterval(interval);
     document.getElementById("headline").innerText = "Terminé !";
